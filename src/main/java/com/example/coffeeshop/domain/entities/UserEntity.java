9bc4c4ba33683ffc029484a49a9db73cbca3c044
MartinKrystev <martin.krystev@gmail.com>
2024-01-02 18:57:58 +0200

    User Entity

---
+++ src/main/java/com/example/coffeeshop/domain/entities/UserEntity.java
@@ -0,0 +1,121 @@
+package com.example.coffeeshop.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Size(min = 5, max = 20)
+    @NotNull
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Size(min = 5, max = 20)
+    @NotNull
+    private String lastName;
+
+    @Column
+    @Size(min = 3)
+    @NotNull
+    private String password;
+
+    @Column(unique = true)
+    @Email
+    @NotNull
+    private String email;
+
+    @OneToMany(mappedBy = "employee")
+    private List<OrderEntity> orders;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String firstName, String lastName, String password, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public List<OrderEntity> getOrders() {
+        return orders;
+    }
+
+    public UserEntity setOrders(List<OrderEntity> orders) {
+        this.orders = orders;
+        return this;
+    }
+
+    public int getAllOrders() {
+        return this.orders.size();
+    }
+}
