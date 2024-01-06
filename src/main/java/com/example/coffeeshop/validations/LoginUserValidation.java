a4e5c9990464247e52791722e0578d7dc5b2de69
MartinKrystev <martin.krystev@gmail.com>
2024-01-06 12:08:31 +0200

    Login User Validation

---
+++ src/main/java/com/example/coffeeshop/validations/LoginUserValidation.java
@@ -0,0 +1,20 @@
+package com.example.coffeeshop.validations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Constraint(validatedBy = LoginUserValidator.class)
+public @interface LoginUserValidation {
+    String message() default "Username or Password doesn't match!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
