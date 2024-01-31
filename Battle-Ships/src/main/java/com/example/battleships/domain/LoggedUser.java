6fc8fbb3b6099bc340e75064ed3b88ccc113b3d0
MartinKrystev <martin.krystev@gmail.com>
2024-01-31 22:53:53 +0200

    Logged User

---
+++ Battle-Ships/src/main/java/com/example/battleships/domain/LoggedUser.java
@@ -0,0 +1,42 @@
+package com.example.battleships.domain.beans;
+
+import com.example.battleships.domain.entities.User;
+
+public class LoggedUser {
+    private Long id;
+
+    private String username;
+
+    public LoggedUser() {
+    }
+
+    public LoggedUser(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void clearFields() {
+        this.id = null;
+        this.username = null;
+    }
+
+    public boolean isLogged() {
+        return this.getId() != null;
+    }
+}
