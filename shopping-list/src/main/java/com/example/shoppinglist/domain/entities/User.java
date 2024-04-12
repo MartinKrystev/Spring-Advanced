f966a89cf4efdb64a2ed66f0c619a4c4d155b118
MartinKrystev <martin.krystev@gmail.com>
2024-04-12 10:15:10 +0300

    User Entity

---
+++ shopping-list/src/main/java/com/example/shoppinglist/domain/entities/User.java
@@ -0,0 +1,35 @@
+package com.example.shoppinglist.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @Column
+    @Size(min = 3, max = 20)
+    private String password;
+
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+}
