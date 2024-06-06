d2220a58ea6a53c7ffdda242c66e9be8bc73e147
MartinKrystev <martin.krystev@gmail.com>
2024-06-06 23:23:42 +0300

    BeanConfig

---
+++ spotify-playlist/src/main/java/com/example/spotifyplaylist/config/BeanConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.spotifyplaylist.config;
+
+import com.example.spotifyplaylist.domain.beans.LoggedUser;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    @SessionScope
+    public LoggedUser loggedUser() {
+        return new LoggedUser();
+    }
+}
