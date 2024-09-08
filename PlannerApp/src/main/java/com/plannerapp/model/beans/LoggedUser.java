bb43a4cd70cfe71be32c4da509eff87c6c52817d
MartinKrystev <martin.krystev@gmail.com>
2024-09-08 22:17:19 +0300

    LoggedUser

---
+++ PlannerApp/src/main/java/com/plannerapp/model/beans/LoggedUser.java
@@ -0,0 +1,42 @@
+package com.plannerapp.model.beans;
+
+import com.plannerapp.model.entity.UserEntity;
+
+public class LoggedUser {
+    private Long id;
+
+    private String username;
+
+    public LoggedUser() {
+    }
+
+    public LoggedUser(UserEntity user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void clearFields() {
+        this.id = null;
+        this.username = null;
+    }
+
+    public boolean isLogged() {
+        return this.getId() != null;
+    }
+}
